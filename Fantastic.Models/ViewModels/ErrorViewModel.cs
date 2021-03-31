13c46386b9f663bf0042462e40ad3ae74c7ff2a1
Jeremiah Ogunbor <jerryogunbor@gmail.com>
2021-03-31 01:43:09 +0100

    Add project files.

---
+++ Fantastic.Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fantastic.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Fantastic.Utility/SD.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fantastic.Utility
+{
+    class SD
+    {
+    }
+}
